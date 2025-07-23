26e495358eda27ed2695712c04db97fb284652a5
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-07-23 09:33:18 +0000

    Basic views, templates, static setup

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,12 @@ from django.http import Http404, HttpResponseRedirect
 
 
 def blog_index(request):
-    pass
+    return render(request, "blog/index.html")
 
 
 def all_posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 
-def post_detail (request):
-    pass
+def post_detail (request, slug):
+    return render(request, "blog/post-detail.html")
