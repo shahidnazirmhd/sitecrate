70f8c3f66371ac0252619c63f775f59c9f0894a3
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-08-01 08:16:27 +0000

    Setup Tags in view & add favicon

--- blog/views.py
+++ blog/views.py
@@ -97,5 +97,6 @@ def all_posts(request):
 def post_detail (request, slug):
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-      "post": identified_post
+      "post": identified_post,
+      "post_tags": identified_post.tags.all()
     })
